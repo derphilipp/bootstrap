a705e4f632b6c7e82294180d3f73aee2b7979f01
Philipp Weißmann <mail@philipp-weissmann.de>
2018-10-09 19:49:10 +0200

    Fix linting issues, add pre-commit, yamllint, ansible-lint, black

--- macos.sh
+++ macos.sh
@@ -4,6 +4,6 @@ command -v brew   2>/dev/null >/dev/null || (/usr/bin/ruby -e "$(curl -fsSL http
 command -v pipenv 2>/dev/null >/dev/null || brew install pipenv
 pipenv install --three
 pipenv run ansible-galaxy install -r requirements.yml  --roles-path=./roles
-pipenv run ansible-playbook playbook.yml -i inventory.yml --extra-vars="ansible_python_interpreter=$(pipenv run which python)"
+pipenv run ansible-playbook playbook.yml -i inventory --extra-vars="ansible_python_interpreter=$(pipenv run which python)"
 echo "Done"
 
